2a973bf62f1e01c6642c6cf4e3f010c076780ffb
Joseph Yølk Chiocchi <joe@yolk.cc>
2015-10-30 18:34:29 -0500

    will investigate

--- test.js
+++ test.js
@@ -91,9 +91,33 @@ test('multiple json records', t => {
 });
 
 // test('multiple records mixed', t => {
-//   var sampleMultiRecordSNS = sampleMessageSNS;
-//   sampleMultiRecordSNS.Records[1] = sampleMultiRecordSNS.Records[0];
-//   sampleMultiRecordSNS.Records[1].Sns.Message = JSON.stringify(sampleJSON);
-//   t.is(fn(sampleMultiRecordSNS), [messageString, sampleJSON]);
+//   const messageString = "Hello from SNS!";
+//   var sampleMultiRecordSNS = {"Records": [{"EventVersion": "1.0", "EventSubscriptionArn": "arn:aws:sns:EXAMPLE", "EventSource": "aws:sns", "Sns": {"SignatureVersion": "1", "Timestamp": "1970-01-01T00:00:00.000Z", "Signature": "EXAMPLE", "SigningCertUrl": "EXAMPLE", "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e", "Message": "Hello from SNS!", "MessageAttributes": {"Test": {"Type": "String", "Value": "TestString"}, "TestBinary": {"Type": "Binary", "Value": "TestBinary"} }, "Type": "Notification", "UnsubscribeUrl": "EXAMPLE", "TopicArn": "arn:aws:sns:EXAMPLE", "Subject": "TestInvoke"} } ] };
+//   const sampleJSON = {
+//     a: 'string',
+//     b: 'anotherString',
+//     c: ['a', 'b', 'c'],
+//     d: [{
+//       a: 'string',
+//       b: ['a', 'b', 'c'],
+//       c: {
+//         a: 'b',
+//         c: 'd'
+//       }
+//     }],
+//     e: {
+//       a: 'a',
+//       b: 'b',
+//       c: ['a', 'b', 'c'],
+//       d: {
+//         a: 'a',
+//         b: 'b'
+//       }
+//     }
+//   };
+//   const sampleJSONString = JSON.stringify(sampleJSON);
+//   sampleMultiRecordSNS.Records.push(sampleMultiRecordSNS.Records[0]);
+//   sampleMultiRecordSNS.Records[1].Sns.Message = sampleJSONString;
+//   t.same(fn(sampleMultiRecordSNS), [messageString, sampleJSON]);
 //   t.end();
 // });
